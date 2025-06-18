f2b69159fd435458dd60134b88658e88c925da71
Rinkoo-Aionos <rinkoo.kandpal@aionos.ai>
2025-06-18 13:24:54 +0530

    chore: remove unnecessary files

--- backend/src/module/index.ts
+++ backend/src/module/index.ts
@@ -5,13 +5,12 @@ import { OkSuccess } from '../shared/utils/CustomSuccess';
 
 import agencyRoutes from './agency/agency.routes';
 import authRoutes from './auth/auth.routes';
-import { leadRoutes } from './lead/lead.routes';
+
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/agency', agencyRoutes);
-router.use('/lead', leadRoutes);
 
 router.get('/health', (req, res) => {
   const uptime = process.uptime();
